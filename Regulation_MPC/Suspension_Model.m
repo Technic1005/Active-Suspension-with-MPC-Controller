32b9a0f6b968c4af85db49df60bfbec250e9176f
Technic1005 <2531868032@qq.com>
2025-03-11 23:09:44 +0100

    Regulation MPC

---
+++ Regulation_MPC/Suspension_Model.m
@@ -0,0 +1,87 @@
+function [LTI, xlb, xub, ulb, uub] = Suspension_Model()
+m_b = 690;
+m_w1 = 40.5;
+m_w2 = 45.4;
+k_s1 = 17e3;
+k_s2 = 22e3;
+c_s1 = 1500;
+c_s2 = c_s1;
+a = 1.25;
+b = 1.51;
+k_u1 = 192e3;
+k_u2 = k_u1;
+I_yy = 1222;
+
+A = [
+    -(c_s1+c_s2)/m_b, (a*c_s1-b*c_s2)/m_b, c_s1/m_b, c_s2/m_b, -k_s1/m_b, -k_s2/m_b, 0, 0;
+    (a*c_s1-b*c_s2)/I_yy, -(a^2*c_s1+b^2*c_s2)/I_yy, -a*c_s1/I_yy, b*c_s2/I_yy, a*k_s1/I_yy, -b*k_s1/I_yy, 0, 0;
+    c_s1/m_w1, -a*c_s1/m_w1, -c_s1/m_w1, 0, k_s1/m_w1, 0, -k_u1/m_w1, 0;
+    c_s2/m_w2, b*c_s2/m_w2, 0, -c_s2/m_w2, 0, k_s2/m_w2, 0, -k_u2/m_w2;
+    1, -a, -1, 0, 0, 0, 0, 0;
+    1, b, 0, -1, 0, 0, 0, 0;
+    0, 0, 1, 0, 0, 0, 0, 0;
+    0, 0, 0, 1, 0, 0, 0, 0
+];
+
+B=[
+    1/m_b, 1/m_b, 0, 0;
+    -a/I_yy, b/I_yy, 0, 0;
+    -1/m_w1, 0, 0, 0;
+    0, -1/m_w2, 0, 0;
+    0, 0, 0, 0;
+    0, 0, 0, 0;
+    0, 0, -1, 0;
+    0, 0, 0, -1
+];
+
+C = [
+    -(c_s1+c_s2)/m_b, (a*c_s1-b*c_s2)/m_b, c_s1/m_b, c_s2/m_b, -k_s1/m_b, -k_s2/m_b, 0, 0;
+    (a*c_s1-b*c_s2)/I_yy, -(a^2*c_s1+b^2*c_s2)/I_yy, -a*c_s1/I_yy, b*c_s2/I_yy, a*k_s1/I_yy, -b*k_s1/I_yy, 0, 0;
+    0, 0, 0, 0, 1, 0, 0, 0;
+    0, 0, 0, 0, 0, 1, 0, 0;
+    0, 0, 0, 0, 0, 0, 1, 0;
+    0, 0, 0, 0, 0, 0, 0, 1
+];
+
+D = [
+    1/m_b, 1/m_b, 0, 0;
+    -a/I_yy, b/I_yy, 0, 0;
+    0, 0, 0, 0;
+    0, 0, 0, 0;
+    0, 0, 0, 0;
+    0, 0, 0, 0;
+];
+%%
+P = ss(A,B,C,D);
+ts=0.01;          % sampling time
+P_dis=c2d(P,ts);  % discrete system P
+LTI.A=P_dis.A;
+LTI.B=P_dis.B;
+LTI.C=P_dis.C;
+LTI.D=P_dis.D;
+
+xlb = [-inf(), -inf(), -inf(), -inf(), -0.15, -0.15, -inf(), -inf()]';
+xub = [inf(), inf(), inf(), inf(), 0.12, 0.12, (m_b*b/(a+b)+m_w1)*9.81/k_s1, (m_b*a/(a+b)+m_w2)*9.81/k_s2]';
+ulb = [-2500, -2500, -inf(), -inf()]';
+uub = [2500, 2500, inf(), inf()]';
+
+% 
+% T = 5;
+% step = 0:ts:T;
+% x0 = [0;0;0;0;0.02;-0.02;0;0];
+% % Force_F, Force_R, M, WheelSpd_F, WheelSpd_R
+% u0 = [0;0;0;0;0];
+% x = x0;
+% 
+% for i=1:length(step)-1
+%     x_new = LTI.A*x(:,end)+LTI.B*u0;
+%     x=[x, x_new];
+% end
+
+% figure()
+% stairs(step, x(6,:)) % 用阶梯图画出状态保持器的效果
+% xlabel('Time [s]')
+% ylabel('State x6')
+% title('Zero-Order Hold Effect')
+% grid on
+end
---
+++ Regulation_MPC/main.m
@@ -0,0 +1,107 @@
+%% Clear up
+clear all
+close all
+clc
+%% Define Suspension Model
+% Define LTI system and boundary
+[LTI, xlb, xub, ulb, uub] = Suspension_Model();
+
+% Model perdictive controller parameters
+dim.nx = size(LTI.A,2);     % state dimension
+dim.nu = size(LTI.B,2);     % input dimension
+dim.ny = size(LTI.D,1);     % output dimension
+dim.N = 5;                  % prediction horizon
+
+% Weight Matrix
+weight.Q = 1e5*eye(8);
+weight.R = 1e-3*eye(4);
+
+% Find LQR.
+[K, P] = dlqr(LTI.A, LTI.B, weight.Q, weight.R);
+weight.P = P;
+K = -K; % Sign convention.
+
+%% Compute X_f.
+
+% Compute X_f based on exercise 4 question 2
+Xn = struct();
+V = struct();
+Z = struct();
+% Reuse code from exercise 4
+[Xn.('lqr'), V.('lqr'), Z.('lqr')] = findXn(LTI.A, LTI.B, K, dim.N, xlb, xub, ulb, uub, 'lqr');
+
+%% Preprocess of MPC
+
+% Reuse code from exercise 3 to calculate cost
+predmod=predmodgen(LTI,dim);     
+% cost = 1/2xTHx+h'x
+[H,h]=costgen(predmod,weight,dim);
+
+% Calculate control constraints
+[A_u, b_u] = hyperrectangle(ulb, uub);
+A_U = [];
+b_U = [];
+% A_U * U <= b_U
+for i=1:dim.N
+    A_U = blkdiag(A_U, A_u);
+    b_U = [b_U;b_u];
+end
+
+% Calculate state constraints
+% A_X * X <= b_X
+[A_X, b_X] = hyperrectangle(xlb, xub);
+
+% Define terminal set
+terminal = Xn.lqr{1};
+% x_N = T_N * x_0 + S_N * U
+T_N = predmod.T(end-dim.nx+1:end,:);
+S_N = predmod.S(end-dim.nx+1:end,:);
+%% Simulate MPC from the initial starting point.
+
+T = 500;    % Simulation steps
+
+% Matrices to store results
+x=zeros(dim.nx,T+1);
+u_rec=zeros(dim.nu,T);
+
+% Initial consition
+LTI.x0 = [-0.05;0.3;2;-2;0.01;-0.012;0.002;-0.002];
+x(:,1)=LTI.x0;
+
+% Receding horizon implementation
+for k=1:T
+    
+    x_0=x(:,k);
+    
+    % Solve the unconstrained optimization problem (with YALMIP)
+    u_con = sdpvar(dim.nu*dim.N,1);          % define optimization variable
+    Constraint=[u_con(3:4:end) == 0;         % Front road input
+                u_con(4:4:end) == 0          % Rear road input
+                A_X * x_0 <= b_X;            % State constraints
+                A_U * u_con <= b_U;          % Input constraints
+                % terminal.A * (T_N * x_0 + S_N * u_con) <=terminal.b;        % Terminal constraints
+        ];                                           % define constraints
+    Objective = 0.5*u_con'*H*u_con+(h*x_0)'*u_con;     % define cost function
+    optimize(Constraint,Objective);                          % solve the problem
+    u_con=value(u_con);                                  % assign the solution
+    
+    % Select the first input only
+    u_rec(:,k)=u_con(1:4);
+
+    % Compute the state/output evolution
+    x(:,k+1)=LTI.A*x_0 + LTI.B*u_rec(:,k);
+    clear u_con
+    
+end
+
+%%
+step = 0:1:T;
+figure()
+stairs(step, x(1,:)) % 用阶梯图画出状态保持器的效果
+xlabel('Step')
+ylabel('State x1')
+
+figure()
+stairs(step(1:end-1), u_rec(1,:)) % 用阶梯图画出状态保持器的效果
+xlabel('Step')
+ylabel('Input u1')
