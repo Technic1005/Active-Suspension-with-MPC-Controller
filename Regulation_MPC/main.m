6491a7d25580c375738714ad72bf96591ab5ce12
Technic1005 <2531868032@qq.com>
2025-03-21 16:31:46 +0100

    Add pitch moment into control variables

--- Regulation_MPC/main.m
+++ Regulation_MPC/main.m
@@ -13,8 +13,8 @@ dim.ny = size(LTI.D,1);     % output dimension
 dim.N = 5;                  % prediction horizon
 
 % Weight Matrix
-weight.Q = 1e5*eye(8);
-weight.R = 1e-3*eye(4);
+weight.Q = diag([1e4, 1e4, 1e3, 1e3, 1e6, 1e6,1e3,1e3]);
+weight.R = diag([1e-4, 1e-4, 1e-3, 1e-3, 1e-3]);
 
 % Find LQR.
 [K, P] = dlqr(LTI.A, LTI.B, weight.Q, weight.R);
@@ -58,7 +58,7 @@ T_N = predmod.T(end-dim.nx+1:end,:);
 S_N = predmod.S(end-dim.nx+1:end,:);
 %% Simulate MPC from the initial starting point.
 
-T = 500;    % Simulation steps
+T = 200;    % Simulation steps
 
 % Matrices to store results
 x=zeros(dim.nx,T+1);
@@ -68,6 +68,14 @@ u_rec=zeros(dim.nu,T);
 LTI.x0 = [-0.05;0.3;2;-2;0.01;-0.012;0.002;-0.002];
 x(:,1)=LTI.x0;
 
+%% Without MPC Controller
+x_original = LTI.x0;
+u_original = [0;0;0;0;2300];
+for k=1:T
+    x_new = LTI.A * x_original(:,k) + LTI.B * u_original;
+    x_original = [x_original, x_new];
+end
+%%
 % Receding horizon implementation
 for k=1:T
     
@@ -75,18 +83,19 @@ for k=1:T
     
     % Solve the unconstrained optimization problem (with YALMIP)
     u_con = sdpvar(dim.nu*dim.N,1);          % define optimization variable
-    Constraint=[u_con(3:4:end) == 0;         % Front road input
-                u_con(4:4:end) == 0          % Rear road input
+    Constraint=[u_con(3:5:end) == 0;         % Front road input
+                u_con(4:5:end) == 0          % Rear road input
+                u_con(5:5:end) == 2300          % Pitch Moment
                 A_X * x_0 <= b_X;            % State constraints
                 A_U * u_con <= b_U;          % Input constraints
-                % terminal.A * (T_N * x_0 + S_N * u_con) <=terminal.b;        % Terminal constraints
+                terminal.A * (T_N * x_0 + S_N * u_con) <=terminal.b;        % Terminal constraints
         ];                                           % define constraints
     Objective = 0.5*u_con'*H*u_con+(h*x_0)'*u_con;     % define cost function
     optimize(Constraint,Objective);                          % solve the problem
     u_con=value(u_con);                                  % assign the solution
     
     % Select the first input only
-    u_rec(:,k)=u_con(1:4);
+    u_rec(:,k)=u_con(1:5);
 
     % Compute the state/output evolution
     x(:,k+1)=LTI.A*x_0 + LTI.B*u_rec(:,k);
@@ -96,11 +105,14 @@ end
 
 %%
 step = 0:1:T;
+index = 6;
 figure()
-stairs(step, x(1,:)) % 用阶梯图画出状态保持器的效果
+hold on
+stairs(step, x(index,:)) % 用阶梯图画出状态保持器的效果
+stairs(step, x_original(index,:))
 xlabel('Step')
 ylabel('State x1')
-
+%%
 figure()
 stairs(step(1:end-1), u_rec(1,:)) % 用阶梯图画出状态保持器的效果
 xlabel('Step')
